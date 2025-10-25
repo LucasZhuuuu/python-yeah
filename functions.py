8a9e85f115efce7ae02341d8171840cfa347ca70
LucasZhuuuu <lucasziluwee@gmail.com>
2025-10-25 14:54:03 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -24,27 +24,73 @@
 
 
 
-class calculator:
-    numbers = list[int]
+# class calculator:
+#     numbers = list[int]
     
-    def numbers_calc(self, num : int):
-        self.numbers.append(num) # this function is for adding the numbers that the user inputed to the list
+#     def numbers_calc(self, num : int):
+#         self.numbers.append(num) # this function is for adding the numbers that the user inputed to the list
 
-    def add(self) -> int: # add is a function, when we do this function we add up the numbers in the list
-        result = 0
-        for num in self.numbers:
-            result += num
-        return result
+#     def add(self) -> int: # add is a function, when we do this function we add up the numbers in the list
+#         result = 0
+#         for num in self.numbers:
+#             result += num
+#         return result
     
-    def sub(self) -> int: # sub is also a function, this function must start with a number so we take the number at index 0 
-        result = self.numbers[0] # start subtracting from the first number in the self.numbers list
-        for num in self.numbers:
-            result -= num
-        return result
-    print("Give me as many numbers as you want, after, type end")
-    int(input())
+#     def sub(self) -> int: # sub is also a function, this function must start with a number so we take the number at index 0 
+#         result = self.numbers[0] # start subtracting from the first number in the self.numbers list
+#         for num in self.numbers:
+#             result -= num
+#         return result
+#     print("Give me as many numbers as you want, after, type end")
+#     int(input())
 
 
     
+# def greet(name : str) -> str:
+#     print(f"Greetings {name}!")
+#     return f"Name is {name}" 
+
+# greet("Lucas")
+
+# def c_to_f(celsius : int) -> int: # c to f is a function that we have defined. Celsius would be inputed as an integer
+#     fahrenheit = (celsius * (9/5)) + 32 # what we want the function to do
+#     print (fahrenheit) # print the result
+
+# c_to_f(100) # sets what the input is
+
+# def f_to_c(fahr : int) -> int:
+#     result =  (fahr - 32)/(5*9)
+#     print(result)
+
+# f_to_c(32)
+
+# import random   
+
+# verb = ["jump on a pogo stick", "macarena", "fly", "breakdance", "drive their RAM 1500", "leap", "chase after kids", "set fire"]
+# place = ["park", "mall", "cemetary", "restaurant", "coffee shop", "CN tower", "daycare"]
+
+
+# def story_gen(noun: str):
+#     print(f"{noun}, wants to {random.choice(verb)} to the {random.choice(place)}!")
+#     print(f"Then, {noun} wants to {random.choice(verb)} to the {random.choice(place)}, yayy!")
+    
+# print("What do you want your main character's name to be?")
+
+# noun = str(input())
+
+# story_gen(noun)
+import random
+
+magicians = ["Grandma", "Bob", "Adam"]
+spells = ["Fireball", "Shadow Pull", "Stub your toe"]
+
+def magician_spell(noun : str):
+    print(f"{random.choice(magicians)} casts {random.choice(spells)}")
+
+magician_spell("noun")
+
+
+    
+
 
 
